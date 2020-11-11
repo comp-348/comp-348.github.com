0d402b5a48bd9ac0ebff8542cc2ef0dd557f5956
Philip Dumaresq <phdumaresq@protonmail.com>
2020-11-10 20:22:53 -0500

    Added build settings for exercises and documented the build a little more

--- build.el
+++ build.el
@@ -1,3 +1,21 @@
+;;; build.el --- Build system for comp-348 website
+;;; Commentary:
+;; This is the build system for the COMP-348 website.
+;;
+;; While not strictly necessary, each subdirectory that we want to be building
+;; has it's own variable dedicated to it. The variables are an association list
+;; composed of the options for how to build that directory.
+;;
+;; `:base-directory' is the directory to be build
+;; `:publishing-directory' is the directory for build to be output to
+;; `:section-numbers' Whether or not the headers are numbers
+;; `:table-of-contents' Is the table of contents should be included in the output
+;; `:publishing-function' The Elisp function to be called to publish the build
+;; `:base-extension' The extensions for the files to be published
+;; `:components' A list of all the components belonging to a single build
+;;
+;;; Code:
+
 (setq org-html-htmlize-output-type 'css)
 
 (defvar comp-348-build-root
@@ -8,7 +26,15 @@
     :table-of-contents nil
     :publishing-function org-html-publish-to-html))
 
-(defvar comp-348-build-assets 
+(defvar comp-348-build-examples
+  `("comp-348.github.io:examples"
+    :base-directory ,(concat default-directory "/examples") ; Emacs current file directory
+    :publishing-directory ,(concat default-directory "/docs/examples")
+    :section-numbers nil
+    :table-of-contents nil
+    :publishing-function org-html-publish-to-html))
+
+(defvar comp-348-build-assets
   `("comp-348.github.io:assets"
     :base-directory ,(concat default-directory "/assets")
     :publishing-directory ,(concat default-directory "/docs/assets")
@@ -20,4 +46,9 @@
 (add-to-list 'org-publish-project-alist
              '("comp-348.github.io"
                :components ("comp-348.github.io:root"
+                            "comp-348.github.io:examples"
                             "comp-348.github.io:assets")))
+
+(provide 'build)
+
+;;; build.el ends here
